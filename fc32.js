a7c6b16da20b9db76f3b0ddaba89b92678f98fa4
adamovic6666 <65610144+adamovic6666@users.noreply.github.com>
2022-03-30 19:42:13 +0200

    Update and rename fc29.js to fc32.js

--- fc32.js
+++ fc32.js
@@ -131,8 +131,13 @@ window.addEventListener('DOMContentLoaded', () => {
       removeDropdown(dd3);
       if (!ev.target.classList.contains("dropdown-element")) {
           allInputs.forEach( input => {
-            if(input.value === '') { input.value = ''; return; }
-              input.value = `${dropdownValues[0].postcode} ${dropdownValues[0].name}`;
+            if(input.value === '') { 
+	       input.value = ''; 
+	       return; 
+	    }
+            if(input.value.length === 5) {
+	       input.value = `${dropdownValues[0].postcode} ${dropdownValues[0].name}`;
+            }
             postalCodeIsValid = true;
           })
         }
