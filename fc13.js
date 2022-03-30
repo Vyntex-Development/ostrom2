3daeba492697c895ccc2a3d2251a9cf6e25205b5
adamovic6666 <65610144+adamovic6666@users.noreply.github.com>
2022-03-30 13:36:02 +0200

    Update and rename fc11.js to fc13.js

--- fc13.js
+++ fc13.js
@@ -88,7 +88,7 @@
         const response = await fetch(`https://api.ostrom.de/v1/addresses/cities?zip=${code}`);
         const data = await response.json();
         if (data[0] === null) {
-        	let msg = language() === 'de' ? 'Diese Postleitzahl existiert nicht' : "This postal code doesn't exist";
+        	let msg = language() === 'de' ? 'Postleitzahl existiert nicht' : "Post code does not exist";
           setError(error, msg);
           postalCodeIsValid = false;
           return;
@@ -105,7 +105,7 @@
           setError(error, "");
           dropdownValues = [];
         } else {
-          let msg = language() === 'de' ? 'Die Postleitzahl muss fünfstellig sein' : "Postal code must have five digits";
+          let msg = language() === 'de' ? 'Die Postleitzahl muss fünfstellig sein' : "Post code must have 5 digits";
           if (firstFetch && input.length !== 0) setError(error, msg);
           removeAndClearDropdown(dropdown);
           input.value = ev.target.value;
@@ -158,7 +158,7 @@
       buttonCalculate.addEventListener('click', e => {
         e.preventDefault()
         let inputLength = input.value.trim().length;
-        let msg = language() === 'de' ? 'Die Postleitzahl muss fünfstellig sein' : "Postal code must have five digits";
+        let msg = language() === 'de' ? 'Die Postleitzahl muss fünfstellig sein' : "Post code must have 5 digits";
         if(inputLength < 5) { setError(error, msg); return;}
         if(!postalCodeIsValid) return;
         if (+kwhInput.value > 15000 || +kwhInput.value < 600) { displayError(error2,  language() === 'de' ? "Muss zwischen 600 und 15000 kWh liegen" : "Must be between 600 and 15000 kWh")
