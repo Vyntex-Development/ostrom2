f5ba268997201bd670e9a58f9abc5dc94ee96eaf
adamovic6666 <65610144+adamovic6666@users.noreply.github.com>
2022-03-31 14:20:33 +0200

    Update and rename fc37.js to fc39.js

--- fc39.js
+++ fc39.js
@@ -100,7 +100,7 @@ window.addEventListener('DOMContentLoaded', () => {
         setError(error, "");
         dropdownValues = [];
       } else {
-        let msg = language() === 'de' ? 'Die Postleitzahl muss fünfstellig sein' : "Postalcode must have 5 digits";
+        let msg = language() === 'de' ? 'Die Postleitzahl muss fünfstellig sein' : "Postalcode must have 5";
         if (firstFetch && input.length !== 0) setError(error, msg);
         removeAndClearDropdown(dropdown);
         input.value = ev.target.value;
@@ -154,7 +154,7 @@ window.addEventListener('DOMContentLoaded', () => {
     buttonCalculate.addEventListener('click', e => {
       e.preventDefault()
       let inputLength = input.value.trim().length;
-      let msg = language() === 'de' ? 'Die Postleitzahl muss fünfstellig sein' : "Postalcode must have 5 digits";
+      let msg = language() === 'de' ? 'Die Postleitzahl muss fünfstellig sein' : "Postalcode must have 5";
       if(inputLength < 5) { setError(error, msg); return;}
       if(!postalCodeIsValid) return;
       if (+kwhInput.value > 15000 || +kwhInput.value < 600) { displayError(error2,  language() === 'de' ? "Muss zwischen 600 und 15000 kWh liegen" : "Must be between 600 and 15000 kWh")
